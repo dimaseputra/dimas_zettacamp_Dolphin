ae4d949cda7cea1dc198b409e82a5b7f5beabd53
dimaseputra <dimas.putra@zettacamp.pro>
2023-08-25 13:18:12 +0800

    js day 1

---
+++ day9-node/favBooks.js
@@ -0,0 +1,27 @@
+let myFirstFavoriteBook = 'perihal gendis';
+const mySecondFavoriteBook = 'Laskar Pelangi';
+
+myFirstFavoriteBook = 'bumi manusia';
+// mySecondFavoriteBook = 'kodok lompat';
+
+// console.log(myFirstFavoriteBook);
+// console.log(mySecondFavoriteBook);
+
+let myBooks = myFirstFavoriteBook + ' & ' + mySecondFavoriteBook;
+
+// console.log('this is my books : ' + myBooks);
+
+let countBooks = 2;
+let isGoodBook = true;
+// console.log(countBooks + ' ' + isGoodBook);
+// console.log(typeof countBooks);
+// console.log(typeof isGoodBook);
+
+const perihalGendis = {
+  author: 'sapardi djoko d',
+  year: 2010,
+};
+
+// console.log(perihalGendis);
+
+console.log(' ');
